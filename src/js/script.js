75c1d6fce800713bbc02a674e32517dd16f2ed95
alex <zxc080294@gmail.com>
2019-09-02 21:32:21 +0700

    tabs part1

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,28 @@
-$(document).ready(function(){
-	$('.carousel__inner').slick({
-		infinite: true,
-		speed: 400,
-		prevArrow:'<button type="button" class="slick-prev"><img src="icon/arrow.png"</button>',
-		nextArrow:'<button type="button" class="slick-next"><img src="icon/arrow-right.png"</button>',
-		centerMode: true,
-		variableWidth: true,
-		responsive:[
-			{
-			breakpoint: 768,
-			settings:{
-				dots: true,
-				arrows: false
-			}
-			}
+// $(document).ready(function(){
+// 	$('.carousel__inner').slick({
+// 		infinite: true,
+// 		speed: 400,
+// 		prevArrow:'<button type="button" class="slick-prev"><img src="icon/arrow.png"></button>',
+// 		nextArrow:'<button type="button" class="slick-next"><img src="icon/arrow-right.png"></button>',
+// 		centerMode: true,
+// 		variableWidth: true,
+// 		responsive:[
+// 			{
+// 			breakpoint: 1000,
+// 			settings:{
+// 				dots: true,
+// 				arrows: false,
+				
+// 			}
+// 			}
 
-		]
+// 		]
 		
 		   
 		
 		
-	   });
-  });
+// 	   });
+//   });
 	
   // // centerMode: true,
 		// autoplay: true,
@@ -34,3 +35,25 @@ $(document).ready(function(){
 		// infinite: true,
 		 //   setting-name: setting-value
 		 // slidesToShow: 1,
+
+const slider = tns({
+container: '.carousel__inner',
+items: 1,
+slideBy: 'page',
+autoplay: false,
+controls: false,
+nav: true,
+navPosition: 'bottom',
+autoplayHoverPause: true,
+autoplayButtonOutput: false,
+autoplayTimeout:'5000',
+
+
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+	slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+	slider.goTo('next');
+});
